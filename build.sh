a5c0fb3b4e5e574b8d2d488ea0513d192e3836cd
lybo <lyberis.g@gmail.com>
2018-07-30 11:19:30 +0200

    added fieldset to phrase form

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo '-------------------- DOWNLOADING NEW BUILD --------------------' &&
 git pull --rebase origin master &&
+echo '-------------------- MOVING BUILD FOLDER --------------------' &&
 rm -r ../dixme/server/build &&
 cp -r ./build/ ../dixme/server/ &&
-echo 'DONE'
-
+echo '-------------------- DONE  --------------------'
